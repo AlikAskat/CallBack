877fe60067cc5e6f9c30d0e3d8b6c2cf935a4296
Alik Rakhmatullin <a-r-b@bk.ru>
2020-06-03 13:30:15 +0500

    Initial commit

---
+++ src/test/java/ru/netology/CallBackTest.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CallBackTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
