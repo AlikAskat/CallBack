a246c6bdf6a64582358fea7b1d337718271d0e7f
Alik Rakhmatullin <a-r-b@bk.ru>
2020-06-04 13:35:54 +0500

    Update files
    CallBackTest.java, build.gradle, gradle.xml

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,8 +1,10 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CallBackTest {
@@ -13,5 +15,27 @@ class CallBackTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+     public void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+    }
+
+    private void open(String s) {
+
+    }
+
+    private Condition[] exactText(String s) {
+        return new Condition[0];
+    }
+
+    private SelenideElement $(String s) {
     }
 }
