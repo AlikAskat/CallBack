3c3d63c2229650eb609700509d7c7bb2c65a887d
Alik Rakhmatullin <a-r-b@bk.ru>
2020-06-15 13:36:20 +0500

    Update CallBackTest.java

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -13,12 +13,12 @@ class CallBackTest {
     @Test
      public void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name']input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id='agreement']").click();
         form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $("order-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
     }
 
 }
