590de7ab8c712c176e0df52f9d745e642ef16d26
Alik Rakhmatullin <a-r-b@bk.ru>
2020-06-16 12:23:27 +0500

    Update CallBackTest.java

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,9 +1,9 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,7 +18,7 @@ class CallBackTest {
         form.$("[name='phone']").setValue("+79270000000");
         form.$("[class='checkbox__box']").click();
         form.$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $("[data-test-id='order-success']").shouldHave(new Text("Ваша заявка успешно отправлена!"));
     }
 
 }
